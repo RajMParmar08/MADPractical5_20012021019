038c56d577084f86a8467e77ac7c9205b19a0b7e
starkk3000 <rajmpamar08@gmail.com>
2022-09-20 23:45:31 +0530

    p5

--- app/src/main/java/com/example/madpractical5_20012021019/MainActivity.kt
+++ app/src/main/java/com/example/madpractical5_20012021019/MainActivity.kt
@@ -3,32 +3,33 @@ package com.example.madpractical5_20012021019
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.madpractical5_20012021019.databinding.ActivityMainBinding
+import android.view.View
+import com.example.madpractical5_20012021019.MediaService
+import com.example.madpractical5_20012021019.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var playbtn : FloatingActionButton
+    private lateinit var stopbtn : FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        setSupportActionBar(binding.toolbar)
+        playbtn = findViewById(R.id.btn_play)
+        playbtn.setOnClickListener(View.OnClickListener {
+            play()
+        })
+
+        stopbtn = findViewById(R.id.btn_stop)
+        stopbtn.setOnClickListener(View.OnClickListener {
+            stop()
+        })
     }
 
-    private fun play() {
-        Intent(applicationContext, MediaService::class.java).putExtra(
-            MediaService.DATA_KEY,
-            MediaService.DATA_VALUE
-        ).apply {
-            startService(this)
-        }
+    private fun play(){
+        Intent(applicationContext, MediaService::class.java).putExtra(MediaService.DATA_KEY,MediaService.DATA_VALUE).apply { startService(this) }
     }
-
-    private fun stop() {
-        Intent(applicationContext, MediaService::class.java).apply {
-            stopService(this)
-        }
-
-
+    private fun stop(){
+        Intent(applicationContext,MediaService::class.java).apply { stopService(this) }
     }
 }
